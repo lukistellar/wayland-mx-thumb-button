fdb5b820a10e168d3fed01dd450b7e9fbba06343
lukistellar <luki@stellar.re>
2020-04-09 13:12:21 +0200

    initial commit

--- mousemapper.sh
+++ mousemapper.sh
@@ -1,54 +1,31 @@
 #!/bin/bash
 
+thumb_button_id="-1"
+key="KEY_LEFTMETA"
 event_type=EV_KEY
-action_type=POINTER_BUTTON
-pressed="pressed,"
+action_type=KEYBOARD_KEY
+pressed="pressed"
+devices=$(libinput list-devices | grep MX -A1 |grep event | awk '{print $2}')
+count=0
 
-readarray -t devices <<<$(libinput list-devices | grep pointer -B3 | grep -o '/dev/input/event[1-9]*')
-
-# COMMANDS MAP
-BTN_EXTRA=(KEY_LEFTMETA KEY_PAGEUP)
-BTN_SIDE=(KEY_LEFTMETA KEY_PAGEDOWN)
-
-function pressKey(){
-    device=$1; key=$2; value=$3
-    echo "pressing ${key} ${value}"
-    evemu-event /dev/input/${device} --sync --type ${event_type} --code ${key} --value ${value};
-}
-
-function pressCommand(){
-    device=$1; button=$2; movement=$3
-    var=$button[@]
-    command=${!var}
-
-    if [ ${movement} = ${pressed} ]; then
-        for key in ${command}; do
-            pressKey ${device} ${key} 1
-        done
-    else
-        for key in ${command}; do
-            pressKey ${device} ${key} 0
-        done | tac
-    fi
-}
-
-function parseEventLine(){
-    device=$1
-    action=$2
-    button=$4
-    movement=$6
-
-    # compute only if right action
-    if [ ${action} = ${action_type} ]; then
-        pressCommand ${device} ${button} ${movement}
-    fi
-}
 
 function mapDevice(){
-    device=$1
-    while read line; do
-        parseEventLine ${line}
-    done < <(stdbuf -oL libinput debug-events --device ${device} & )
+	device=$1
+	while read line; do
+		action=$(echo "${line}" |awk '{print $2}')
+		button=$(echo "${line}" |awk '{print $5}')
+		movement=$(echo "${line}" |awk '{print $6}')
+		if [ ${action} = ${action_type} ] && [ ${button} = "(${thumb_button_id})" ]; then
+			count=$(($count +1))
+			if [ ${count} = 3 ]; then
+				echo "press key ${key}"
+				evemu-event ${devices} --sync --type ${event_type} --code ${key} --value 1;
+				sleep 0.01
+	                        evemu-event ${devices} --sync --type ${event_type} --code ${key} --value 0;
+				count=0
+			fi
+		fi
+	done < <(stdbuf -oL libinput debug-events --device ${device} & )
 }
 
 for device in ${devices[@]}; do
@@ -56,3 +33,4 @@ for device in ${devices[@]}; do
 done
 
 wait
+
